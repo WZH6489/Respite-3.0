15671c43c7fee3f94c2b60271da0ad49ac7b9010
WH6489 <williamhuang129@yahoo.com>
2026-03-15 12:31:00 -0700

    Initial Commit

---
+++ Respite 3.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Respite 3.0
+//
+//  Created by William Huang on 2026-03-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Respite 3.0/Respite_3_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Respite_3_0App.swift
+//  Respite 3.0
+//
+//  Created by William Huang on 2026-03-15.
+//
+
+import SwiftUI
+
+@main
+struct Respite_3_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
